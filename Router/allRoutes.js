9a4c6eafd458636a5920d881c8a11af204caf7e3
Florian <nairolf7793@yahoo.com>
2024-10-23 14:59:41 +0200

    feat(account): create account.html + optimize signin and signup + create route account

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,9 @@ import Route from "./Route.js";
 export const allRoutes = [
 
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
 
 ];
 
