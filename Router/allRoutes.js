d817a6fe78975777d09cd0350d0bc7eb0072b8c6
Florian <nairolf7793@yahoo.com>
2024-10-25 18:02:14 +0200

    feat(router): add route for signin

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,7 @@ export const allRoutes = [
 
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/gallerie", "Gallerie", "/pages/gallerie.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html" , "/js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html" , "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement mot de passe", "/pages/auth/editPassword.html"),
