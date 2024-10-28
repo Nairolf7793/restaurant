526dc61e2045571566808e5529fd875da005958b
Florian <nairolf7793@yahoo.com>
2024-10-28 15:04:28 +0100

    feat(allroutes): add array in routes for authorize

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,14 +4,14 @@ import Route from "./Route.js";
 
 export const allRoutes = [
 
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/gallerie", "Gallerie", "/pages/gallerie.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html" , "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html" , "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Changement mot de passe", "/pages/auth/editPassword.html"),
-    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
-    new Route("/reserve", "Reservez", "/pages/reservations/reserve.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/gallerie", "Gallerie", "/pages/gallerie.html", []),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html" , ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html" , ["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html" , ["client", "admin"]),
+    new Route("/editPassword", "Changement mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["client"]),
+    new Route("/reserve", "Reservez", "/pages/reservations/reserve.html", ["client"]),
 
 ];
 
