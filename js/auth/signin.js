4092fafbbbed34bf4f9cbd912ab7075d74188516
Florian <nairolf7793@yahoo.com>
2024-10-30 18:51:28 +0100

    test fetch / api => test ok

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,20 +1,47 @@
 const mailInput = document.getElementById ("EmailInput");
 const passwordInput = document.getElementById ("PasswordInput");
 const btnSignin = document.getElementById ("BtnSignin");
+const signinForm = document.getElementById ("siginForm")
 
 btnSignin.addEventListener ("click", checkCredentials);
 
 function checkCredentials(){
-    if(mailInput.value == "test@mail.fr" && passwordInput.value == "azerty"){
-        const token = "flsdhlfhdslfhvbfnxbjndxk!vbf";
-        setToken(token); //place le token en cookie
 
-        setCookie(roleCookieName, "admin", 7); //pour gérer le role du user
 
-        window.location.replace("/");
-    }
-    else {
-        mailInput.classList.add("is-invalid");
-        passwordInput.classList.add("is-invalid");
-    }
+    let dataForm = new FormData (signinForm); // permet de transformer formulaire en get element en dataform
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json"); 
+
+    let raw = JSON.stringify({ //permet de renvoyer les informations en format json
+        "username": dataForm.get("email"),
+        "password": dataForm.get("mdp")
+    });
+
+    let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("http://127.0.0.1:8000/api/login", requestOptions)
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            else{
+                mailInput.classList.add("is-invalid");
+                passwordInput.classList.add("is-invalid");
+            }
+        })
+        .then(result => {
+            const token = "result.apiToken";
+            setToken(token); //place le token en cookie
+
+            setCookie(roleCookieName, result.roles[0], 7); //pour gérer le role du user
+            window.location.replace("/");
+        })
+
+        .catch((error) => console.error(error));
+
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const tokenCookieName = "accesstoken";
 const signoutBtn = document.getElementById("signout-btn")
+const apiUrl ="http://127.0.0.1:8000/api";
 const roleCookieName = "role";
 
 signoutBtn.addEventListener("click", signout);
